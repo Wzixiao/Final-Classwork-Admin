92de80d1d8f2e308d8a152ab3601972bec24169e
Ran <redamancy0326@foxmail.com>
2022-05-22 20:53:27 +0800

    init 1

---
+++ src/views/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="index">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+ 
+}
+</script>
+
+<style scoped>
+.index{
+  height: 300px;
+  width: 300px;
+  background-color: blue;
+}
+</style>
